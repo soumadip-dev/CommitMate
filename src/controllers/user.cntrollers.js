29086b82d6a2fe1aa61ef93dd67b6af498d8da73
soumadip-dev <soumadipmajila@gmail.com>
2025-01-07 13:13:13 +0530

    feat: add controller for generating user feed

--- src/controllers/user.cntrollers.js
+++ src/controllers/user.cntrollers.js
@@ -1,5 +1,6 @@
 // IMPORTING MODULES
 import { ConnectionRequest } from '../models/connectionRequest.models.js';
+import { User } from '../models/user.models.js';
 // CONTROLLER FOR GETTING ALL PENDING CONNECTION REQUESTS
 const getAllConnectionRequestsController = async (req, res) => {
   try {
@@ -24,7 +25,7 @@ const getAllConnectionRequestsController = async (req, res) => {
   } catch (error) {
     res
       .status(400)
-      .send(`Error fetching connection requests: ${error.message}`);
+      .json({ error: `Error fetching connection requests: ${error.message}` });
   }
 };
 
@@ -62,17 +63,53 @@ const getAllEstablishedConnectionsController = async (req, res) => {
       data,
     });
   } catch (error) {
-    res
-      .status(400)
-      .send(`Error fetching established connections: ${error.message}`);
+    res.status(400).json({
+      error: `Error fetching established connections: ${error.message}`,
+    });
   }
 };
 
 // CONTROLLER FOR GETTING USER FEED
 const getFeedController = async (req, res) => {
   try {
+    // Middleware has added user to req
+    const loggedInUser = req.user;
+
+    // Find all connections involving the logged-in user
+    const userConnections = await ConnectionRequest.find({
+      $or: [
+        {
+          fromUserId: loggedInUser._id,
+        },
+        { toUserId: loggedInUser._id },
+      ],
+    }).select('fromUserId toUserId');
+
+    // Store IDs of users the logged-in user is already connected with
+    const connectedUserIds = new Set();
+
+    userConnections.forEach((connection) => {
+      connectedUserIds.add(connection.fromUserId.toString());
+      connectedUserIds.add(connection.toUserId.toString());
+    });
+
+    // Find users who are not already connected and not the logged-in user
+    const suggestedUsers = await User.find({
+      $and: [
+        { _id: { $nin: Array.from(connectedUserIds) } },
+        { _id: { $ne: loggedInUser._id } },
+      ],
+    }).select('firstName lastName photoUrl age gender about skills');
+
+    // Send the suggested users as the feed
+    res.status(200).json({
+      message: `User feed retrieved successfully for ${loggedInUser.firstName}`,
+      data: suggestedUsers,
+    });
   } catch (error) {
-    res.status(400).send(`Error fetching user feed: ${error.message}`);
+    res
+      .status(400)
+      .json({ error: `Error fetching user feed: ${error.message}` });
   }
 };
 
