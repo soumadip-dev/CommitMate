4cceb585e7d6ff5d69ef16e6501d0c3ce473d881
soumadip-dev <soumadipmajila@gmail.com>
2025-04-13 13:05:02 +0530

    feat: add data sanitization and schema validations

--- src/app.js
+++ src/app.js
@@ -1,30 +1,71 @@
-// Importing
+// IMPORTING MODULES
 import express from 'express';
 import { User } from './models/user.models.js';
 
-// Create an Express application
 const app = express();
-
-// Middleware to parse JSON in request body
 app.use(express.json());
 
-app.post('/signup', async (req, res) => {
+// ROUTE: ADD NEW USER
+app.post('/add', async (req, res) => {
   const userObj = req.body;
 
   try {
     const existingUser = await User.findOne({ emailId: userObj.emailId });
     if (existingUser) {
-      return res.status(400).send('User already present');
+      return res.status(400).json({ message: 'User already present' });
     }
 
     const user = new User(userObj);
     await user.save();
-    res.status(201).send('User added to DB');
+    res.status(201).json({ message: 'User added to DB' });
   } catch (err) {
     console.error('Error during signup:', err);
-    res.status(500).send('Internal Server Error');
+    res.status(500).json({ message: err.message });
   }
 });
 
-// Export the app instance
+// ROUTE: UPDATE USER DETAILS
+app.patch('/update/:userId', async (req, res) => {
+  const userId = req.params?.userId;
+  const userObj = req.body;
+
+  const ALLOWED_UPDATES = ['photoUrl', 'about', 'gender', 'age', 'skills'];
+
+  const isUpdateAllowed = Object.keys(userObj).every((k) => {
+    return ALLOWED_UPDATES.includes(k);
+  });
+
+  const ismaxSkillReach = userObj.skills.length < 10;
+
+  if (!isUpdateAllowed || !ismaxSkillReach) {
+    return res.status(400).json({ message: 'Update not allowed' });
+  }
+
+  try {
+    const user = await User.findByIdAndUpdate(userId, userObj, {
+      runValidators: true,
+      new: true,
+    });
+    res.status(200).json({ message: 'Updation done' });
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ message: 'Updation failed' });
+  }
+});
+
+// ROUTE: DELETE USER
+app.delete('/delete', async (req, res) => {
+  const userId = req.body.userId;
+
+  try {
+    const user = await User.findByIdAndDelete(userId);
+    console.log(user);
+    res.status(200).json({ message: 'Deletion successful' });
+  } catch (err) {
+    console.error(err);
+    res.status(400).json({ message: err.message });
+  }
+});
+
+// EXPORTING APP
 export default app;
--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,55 +1,72 @@
 // IMPORTING MODULES
 import mongoose, { Schema } from 'mongoose';
+import validator from 'validator';
 
 // DEFINE USER SCHEMA
-const userSchema = new Schema({
-  firstName: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  lastName: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  emailId: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  age: {
-    type: Number,
-    min: 16,
-  },
-  gender: {
-    type: String,
-    enum: ['Male', 'Female', 'Other'],
-  },
-  photoUrl: {
-    type: {
-      url: { type: String },
-      localpath: { type: String },
+const userSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
     },
-    default: () => ({
-      url: 'https://placehold.co/600x400',
-      localpath: '',
-    }),
+    lastName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    emailId: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error('Invalid email address');
+        }
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+      validate(value) {
+        if (!validator.isStrongPassword(value)) {
+          throw new Error(
+            'Password must be at least 8 characters long, contain at least one uppercas letter, one lowercase letter, one digit, and one special character',
+          );
+        }
+      },
+    },
+    age: {
+      type: Number,
+      min: 16,
+    },
+    gender: {
+      type: String,
+      enum: ['Male', 'Female', 'Other'],
+    },
+    photoUrl: {
+      type: String,
+      default: 'https://placehold.co/600x400',
+
+      validate(value) {
+        if (!validator.isURL(value)) {
+          throw new Error('Invalid URL');
+        }
+      },
+    },
+    about: {
+      type: String,
+      default: "This user hasn't added any information yet.",
+    },
+    skills: {
+      type: [String],
+      default: [],
+    },
   },
-  about: {
-    type: String,
-    default: "This user hasn't added any information yet.",
-  },
-  skills: {
-    type: [String],
-    default: [],
-  },
-},{timestamps:true});
+  { timestamps: true },
+);
 
 // CREATE AND EXPORT USER MODEL
 export const User = mongoose.model('User', userSchema);
