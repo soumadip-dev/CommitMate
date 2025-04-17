cdfa4331c1c41e97a88d79824791ca29cbff6ec1
soumadip-dev <soumadipmajila@gmail.com>
2025-04-17 17:36:26 +0530

    feat: implement reset password functionality

--- src/controllers/profile.controllers.js
+++ src/controllers/profile.controllers.js
@@ -1,4 +1,6 @@
 // IMPORTING MODULES
+import bcrypt from 'bcryptjs';
+import validator from 'validator';
 import { validateProfileEditData } from '../utils/validations.js';
 
 // CONTROLLER FOR VIEW PROFILE
@@ -54,5 +56,51 @@ const editProfileController = async (req, res) => {
   }
 };
 
+const editProfilePasswordController = async (req, res) => {
+  try {
+    const { password } = req.body;
+
+    // Check if given field is allowed to validate or not
+    if (
+      !validator.isStrongPassword(password, {
+        minLength: 8,
+        minLowercase: 1,
+        minUppercase: 1,
+        minNumbers: 1,
+        minSymbols: 1,
+      })
+    ) {
+      throw new Error(
+        'Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character.',
+      );
+    }
+    // User is already attached to req by authMiddleware
+    const loggedInUser = req.user;
+
+    // Update hashed password only if provided
+    const hashedPassword = await bcrypt.hash(password, 10);
+    loggedInUser.password = hashedPassword ?? loggedInUser.password;
+
+    // Save the changes to the database
+    await loggedInUser.save();
+
+    // after changing the password logout the user
+    res.cookie('token', '', {});
+
+    // Send success response with updated user
+    res
+      .status(200)
+      .json({ message: `Your password has been updated successfully.` });
+  } catch (error) {
+    res
+      .status(400)
+      .json({ error: `Error updating password: ${error.message}` });
+  }
+};
+
 // EXPORTING CONTROLLERS
-export { editProfileController, viewProfileController };
+export {
+  editProfileController,
+  editProfilePasswordController,
+  viewProfileController,
+};
--- src/routes/profile.routes.js
+++ src/routes/profile.routes.js
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import {
   editProfileController,
   viewProfileController,
+  editProfilePasswordController,
 } from '../controllers/profile.controllers.js';
 import authMiddleware from '../middlewares/auth.middlewares.js';
 
@@ -14,6 +15,9 @@ router.get('/view', authMiddleware, viewProfileController);
 
 // EDIT PROFILE ROUTE
 router.patch('/edit', authMiddleware, editProfileController);
+
+// RESET PASSWORD OF PROFILE ROUTE
+router.patch('/resetpassword', authMiddleware, editProfilePasswordController);
 
 // EXPORTING ROUTER INSTANCE
 export default router;
